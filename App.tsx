dadc7a64b1258a4c8e413600a8ea91d1debdca2e
Ben Hur <benhur.martins@hotmail.com>
2023-01-24 13:07:00 -0300

    animate start props

--- App.tsx
+++ App.tsx
@@ -1,15 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Animated, {
   runOnJS,
@@ -22,31 +12,42 @@ const App = () => {
   const color1 = useSharedValue('#FF0000');
   const color2 = useSharedValue('#00FF00');
   const color3 = useSharedValue('#0000FF');
-  const [colors, setColors] = useState([color1.value, color2.value]);
+  const x = useSharedValue(0);
+  const y = useSharedValue(0);
+  const [animatedProps, setAnimatedProps] = useState({
+    colors: [color1.value, color2.value, color3.value],
+    x: 0,
+    y: 0,
+  });
 
   useEffect(() => {
     setInterval(() => {
       const values = [color1.value, color2.value, color3.value];
-      color1.value = withTiming(values[1], {duration: 4900});
-      color2.value = withTiming(values[2], {duration: 4900});
-      color3.value = withTiming(values[0], {duration: 4900});
-    }, 5000);
+      color1.value = withTiming(values[1], {duration: 2000});
+      color2.value = withTiming(values[2], {duration: 2000});
+      color3.value = withTiming(values[0], {duration: 2000});
+      x.value = withTiming(Math.floor(Math.random() * 10) / 10, {
+        duration: 2000,
+      });
+      y.value = withTiming(Math.floor(Math.random() * 10) / 10, {
+        duration: 2000,
+      });
+    }, 2000);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useDerivedValue(() => {
     const result = [color1.value, color2.value, color3.value];
-    runOnJS(setColors)(result);
-  }, [color1.value, color2.value]);
+    runOnJS(setAnimatedProps)({colors: result, x: x.value, y: y.value});
+  }, [color1.value, color2.value, color3.value]);
 
   return (
     <Animated.View style={styles.container}>
       <LinearGradient
-        colors={colors}
+        colors={animatedProps.colors}
         style={styles.linearGradient}
-        start={{x: 0.7, y: 0}}>
-        <Text>Diagonal Gradient</Text>
-      </LinearGradient>
+        start={{x: animatedProps.x, y: animatedProps.y}}
+      />
     </Animated.View>
   );
 };
